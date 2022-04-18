d30a708221a59470bbbaa03196953b205e15202c
Shouldsee <shouldsee@github.com>
2022-04-18 23:34:10 +0800

    rm old version

--- old-hugo-version/content/catsmile/default.md
+++
@@ -1,6 +0,0 @@
----
-title: "Default"
-date: 2022-03-24T13:39:01+08:00
-draft: true
----
-
--- old-hugo-version/start.sh
+++
@@ -1,2 +0,0 @@
-sudo hugo server -w --bind 10.60.108.246 --port 80 .
-#cd content/ && sudo python3 -m http.server 80
