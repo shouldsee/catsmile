dcf2e5382dbff8396435c79c38b5677dc54d00ea
shouldsee <shouldsee@qq.com>
2022-09-29 23:35:45 +0800

    remove markov-lm to avoid confusion

---
+++ markov_lm/nlp/task.sh
@@ -0,0 +1,3 @@
+# bash run.sh --model.model_name DLM142 --model.embed_dim 60 --model.depth 12 --loglr -4 --model.p_null 0.05 --batch_size 150 $@
+
+bash run.sh --model.model_name DLM147 --model.embed_dim 60 --model.depth 4 --loglr -4 --batch_size 22 --model.p_null 0.05 $@
